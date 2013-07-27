f51fb45795d1d48dda2bc52759d4e663a996b760
maxsuhak <maxsuhak@gmail.com>
2013-07-27 17:24:35 +0300

    valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ActiveRecord::Base
   has_many :categories
   has_many :tasks
 
+   validates :first_name, length: { minimum: 2, too_short: "%{count} characters is the minimum allowed" }
+   validates :last_name, length: { minimum: 2, too_short: "%{count} characters is the minimum allowed" }
+   validates :title, length: { maximum: 50, too_long: "%{count} characters is the maximum allowed" }
+   validates :desc, length: { maximum: 350, too_long: "%{count} characters is the maximum allowed" }
+   validates :password, length: { in: 3..20 }
+   validates :first_name, :last_name, :login, :email, presence: true
   def full_name
   	"#{first_name} #{last_name}"
   	# [first_name, last_name].join(" ")
