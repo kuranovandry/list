42bef975e199df88bb78c234b9aab7e21bf12804
maxsuhak <maxsuhak@gmail.com>
2013-07-28 12:52:33 +0300

    valid home

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ActiveRecord::Base
   has_many :categories
   has_many :tasks
 
-   validates :first_name, length: { minimum: 2, too_short: "%{count} characters is the minimum allowed" }
-   validates :last_name, length: { minimum: 2, too_short: "%{count} characters is the minimum allowed" }
-   validates :title, length: { maximum: 50, too_long: "%{count} characters is the maximum allowed" }
-   validates :desc, length: { maximum: 350, too_long: "%{count} characters is the maximum allowed" }
-   validates :password, length: { in: 3..20 }
-   validates :first_name, :last_name, :login, :email, presence: true
+  validates :first_name, length: { minimum: 2, too_short: "%{count} characters is the minimum allowed" }
+  validates :last_name, length: { minimum: 2, too_short: "%{count} characters is the minimum allowed" }
+  validates :title, length: { maximum: 50, too_long: "%{count} characters is the maximum allowed" }
+  validates :desc, length: { maximum: 350, too_long: "%{count} characters is the maximum allowed" }
+  validates :password, length: { in: 3..20 }
+   
+  validates :first_name, :last_name, :login, :email, presence: true
+   
+  validates :email, :first_name, :last_name, uniqueness: true
+
+  validates :first_name, :last_name, :email, uniqueness: { case_sensitive: false }
+  validates :password, uniqueness: { case_sensitive: true }
+  
   def full_name
   	"#{first_name} #{last_name}"
   	# [first_name, last_name].join(" ")
