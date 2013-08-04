e2988ad93d740aa803f00e19999d65486c852531
maxsuhak <maxsuhak@gmail.com>
2013-08-04 16:52:10 +0300

    homework

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,14 @@ class Task < ActiveRecord::Base
   belongs_to :user
  
 
+  def category_name
+  	category.name if category
+  end
+
+  def category_name=(name)
+  	self.category = Category.find_or_create_by_name(name) unless name.blank?
+  end
+
   validates :film_name, :film_duration, length: { in: 3..20 }
 
   validates :film_name, :film_duration, presence: true
